cd6ce12e50f200bdda0897691d302c81a544715f
zabihullah18381 <zabihullah18381@gmail.com>
2023-08-05 16:02:17 +0500

    Model Traning

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-End to End Machine learning Project
+# Student Performance Indicator - Zabih ullah
+
+
+**Problem Statement** 
+- This project understands how the student's performance (`test Score`) is affected by other variables such as Gender, Ethnicity, Parental level of education, Lunch and Test preparation course.
+
+#### Life cycle of Machine learning Project
+
+- Understanding the Problem Statement
+- Data Collection
+- Data Checks to perform
+- Exploratory data analysis
+- Data Pre-Processing
+- Model Training
+- Choose best model
+
+
+
+### Data Collection
+- Dataset Source - https://www.kaggle.com/datasets/spscientist/students-performance-in-exams?datasetId=74977
+- The data consists of 8 column and 1000 rows.
+
+Target variable:
+* `math score`: math score of the student is Target variable.
+
+
+
+# Screenshot of UI
+
+![HomepageUI](./Screenshots/HomepageUI.jpg)
+
+
+
+# Approach for the project 
+
+1. Data Ingestion : 
+    * In Data Ingestion phase the data is first read as csv. 
+    * Then the data is split into training and testing and saved as csv file.
+
+2. Data Transformation : 
+    * In this phase a ColumnTransformer Pipeline is created.
+    * for Numeric Variables first SimpleImputer is applied with strategy median , then Standard Scaling is performed on numeric data.
+    * for Categorical Variables SimpleImputer is applied with most frequent strategy, then ordinal encoding performed , after this data is scaled with Standard Scaler.
+    * This preprocessor is saved as pickle file.
+
+3. Model Training : 
+    * In this phase base model is tested . The best model found was Linear Regression.
+    * After this hyperparameter tuning is performed on catboost and knn model etc.
+    * A final VotingRegressor is created which will combine prediction of catboost, xgboost and , Decision Tree and Linear Regression.
+    * This model is saved as pickle file.
+
+4. Prediction Pipeline : 
+    * This pipeline converts given data into dataframe and has various functions to load pickle files and predict the final results in python.
+
+5. Flask App creation : 
+    * Flask app is created with User Interface to predict the students marks inside a Web Application.
+
+
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -14,7 +14,7 @@ from xgboost import XGBRegressor
 # Importing custom modules for exception handling, logging, and utilities
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_object, evaluate_model
+from src.utils import save_object, evaluate_models
 
 # Defining a data class to hold configuration parameters for model training
 
@@ -50,23 +50,41 @@ class ModelTrainer:
             }
 
             # Dictionary containing hyperparameters for each model
-            params = {
+            params={
                 "Decision Tree": {
-                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
                 },
-                "Random Forest": {
-                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                
+                "Random Forest":{
+                    'n_estimators': [8,16,32,64,128,256]
                 },
-                "Gradient Boosting": {
-                    'learning_rate': [.1, .01, .05, .001],
-                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
-                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,128,256]
                 },
-
+                
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
             }
 
             # Evaluate models with cross-validation and hyperparameter tuning
-            model_report = evaluate_model(
+            model_report = evaluate_models(
                 X_train=X_train,
                 y_train=y_train,
                 X_test=X_test,
@@ -80,6 +98,7 @@ class ModelTrainer:
             best_model_name = list(model_report.keys())[list(
                 model_report.values()).index(best_model_score)]
             best_model = models[best_model_name]
+            
 
             # If the best model's score is below a threshold, raise an exception
             if best_model_score < 0.6:
